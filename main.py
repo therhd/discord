d45b7eb329cce276f6e6554fd8ccbae7b0534e8a
Raymond Dubisky <rdubisky@gmail.com>
2019-10-05 12:28:19 -0400

    initial build, ping pong

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+import config
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+client.run(config.token)

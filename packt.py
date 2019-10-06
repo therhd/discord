ce06907b2520752d1a12b02a88b7fec35ed81641
Raymond Dubisky <rdubisky@gmail.com>
2019-10-06 15:52:23 -0400

    packt will let you find out about future freebies

--- packt.py
+++ packt.py
@@ -1,12 +1,16 @@
 import requests
 import json
+import datetime
 
-offersURL = 'https://services.packtpub.com/free-learning-v1/offers?dateFrom=2019-10-06T00:00:00.000Z&dateTo=2019-10-07T00:00:00.000Z'
+offersURL = 'https://services.packtpub.com/free-learning-v1/offers?dateFrom={0}T00:00:00.000Z&dateTo={1}T00:00:00.000Z'
 summaryURL = 'https://static.packt-cdn.com/products/{}/summary'
 saleURL = 'https://www.packtpub.com/free-learning'
 
-def getDaily():
-    offersReq = requests.get(offersURL)
+def getDaily(date = datetime.datetime.now()):
+    end = date + datetime.timedelta(hours=24)
+    URL = offersURL.format(date.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'))
+
+    offersReq = requests.get(URL)
     offers = json.loads(offersReq.content)
     summary = json.loads(requests.get(summaryURL.format(offers['data'][0]['productId'])).content)
     return({'summary': summary, 'sale': offers['data'][0]})
@@ -15,5 +19,13 @@ def getDailyString():
     deal = getDaily()
     return('Today\'s free item is: {0}\n{1}\nExpires at {2}\n{3}'.format(deal['summary']['title'], deal['summary']['oneLiner'], deal['sale']['expiresAt'], saleURL))
 
+def getFuture(days = 7):
+    for i in range(1, days + 1):
+        date = datetime.datetime.now() + datetime.timedelta(hours=(i * 24))
+        end = date + datetime.timedelta(hours=24)
+        deal = getDaily(date)
+        print('{0} T00:00Z - {1} T00:00Z: {2}'.format(date.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'), deal['summary']['title']))
+
 if __name__ == "__main__":
-    print(getDailyString())
+    #print(getDailyString())
+    getFuture(30)

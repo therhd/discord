30f191970e0cf245ba18d7a5f2fb69771fedf947
rdubisky <rdubisky@med.umich.edu>
2020-03-16 19:22:33 -0400

    MI changed their table format

--- mi_corona.py
+++ mi_corona.py
@@ -9,14 +9,17 @@ def getStats():
         soup = BeautifulSoup(response.content, 'html.parser')
         tables = soup.find_all('table')
         for table in tables:
-            if 'Positive for 2019-nCoV' in str(table):
+            if 'Confirmed COVID-19 Cases by County' in str(table):
                 data = {}
                 for row in table.find_all('tr'):
-                    columns = row.find_all('td')
-                    gross = '\xa0 '
-                    key = str(columns[0].get_text()).replace(gross, '')
-                    val = str(columns[1].get_text()).replace(gross, '')
-                    data.update( {key:val} )
+                    if 'Confirmed COVID-19 Cases by County' not in str(row):
+                        columns = row.find_all('td')
+                        gross = '\xa0 '
+                        key = str(columns[0].get_text()).replace(gross, '')
+                        val = str(columns[1].get_text()).replace(gross, '')
+                        key = key.replace('\n', '')
+                        val = val.replace('\n', '')
+                        data.update( {key:val} )
         return(data)
     else:
         return(None)
@@ -25,10 +28,12 @@ def getStatsString():
     data = getStats()
     string = 'Michigan 2019-nCoV (Covid-19)\n'
     for k,v in data.items():
-        if k == 'Date' or k == 'Negative for 2019-nCoV' or k == 'Positive for 2019-nCoV':
+        if k != 'Total':
             k = k.replace('**', '')
-            string += '{}: **{}**\n'.format(k,v)
+            string += '{}: **{}**, '.format(k,v)
+    string = string.rstrip(', ') + '\nTotal: **{}**'.format(data.get('Total'))
     return string
 
 if __name__ == "__main__":
+    # print(getStats())
     print(getStatsString())

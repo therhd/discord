67faba5412ef5e61f6541157015df1bbb28c9903
rdubisky <rdubisky@med.umich.edu>
2020-03-18 14:26:50 -0400

    changed to new page, back to totals

--- mi_corona.py
+++ mi_corona.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.michigan.gov/coronavirus'
+# url = 'https://www.michigan.gov/coronavirus'
+url = 'https://www.michigan.gov/coronavirus/0,9753,7-406-98163-520743--,00.html'
 
 def getStats():
     response = requests.get(url)
@@ -9,10 +10,10 @@ def getStats():
         soup = BeautifulSoup(response.content, 'html.parser')
         tables = soup.find_all('table')
         for table in tables:
-            if 'Daily Confirmed COVID-19 Cases by County' in str(table):
+            if 'Confirmed COVID-19 Cases by County' in str(table):
                 data = {}
                 for row in table.find_all('tr'):
-                    if 'Daily Confirmed COVID-19 Cases by County' not in str(row):
+                    if 'Confirmed COVID-19 Cases by County' not in str(row):
                         columns = row.find_all('td')
                         if len(columns) > 0:
                             gross = '\xa0 '
@@ -27,7 +28,7 @@ def getStats():
 
 def getStatsString():
     data = getStats()
-    string = 'Michigan 2019-nCoV (Covid-19)\nPositive test results from yesterday:\n'
+    string = 'Michigan 2019-nCoV (Covid-19)\nMore details at {}\n'.format(url)
     for k,v in data.items():
         if k != 'Total' and k != 'County':
             k = k.replace('**', '')
